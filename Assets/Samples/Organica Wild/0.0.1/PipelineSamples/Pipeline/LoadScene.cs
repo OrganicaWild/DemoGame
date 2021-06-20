bfd8bd780439417a1630fe2f4887cd5a5e9e9648
Christoph Vögele <christoph228@gmail.com>
2021-06-20 13:46:48 +0200

    make it load a random scene from the given ones and reimport OW to allow for builds

--- Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/LoadScene.cs
+++ Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/LoadScene.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = System.Random;
 
 public class LoadScene : MonoBehaviour
 {
+    public string[] sceneNames;
+    private List<int> order = new List<int>();
+
     private void Start()
     {
-        SceneManager.LoadScene("NewPipelineTestScene");
+        if (order.Count == 0)
+        {
+            order = new List<int>();
+            for (int i = 0; i < sceneNames.Length; i++)
+            {
+                order.Add(i);
+            }
+            
+            
+            Random rng = new Random();
+            order = order.OrderBy(a => rng.NextDouble()).ToList();
+            
+        }
+
+        string nameOfSceneToLoad =  sceneNames[order.First()];
+        order.RemoveAt(0);
+
+        SceneManager.LoadScene(nameOfSceneToLoad);
     }
 }
