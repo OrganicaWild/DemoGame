59c653114a3a70249c6768c1c66154b42ae6ca38
Christoph Voegele <christoph228@gmail.com>
2021-06-25 13:26:20 +0200

    add floyd-warshall path calculation

--- Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/DataPack.cs
+++ Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/DataPack.cs
@@ -16,12 +16,13 @@ namespace Samples.Organica_Wild._0._0._1.PipelineSamples.Pipeline
         public double timeWalking;
         public double timeAll;
         public int failedActivations;
+        public float averageAreaDistance;
 
         public string ToCsvString()
         {
             return
                 $"{seed},{type},{casual},{pathAverage},{pathSum},{cyclesAmount},{areaSizeAverage}," +
-                $"{landmarkTypesAmount},{colorsAmount},{silhouettesAmount},{timeRunning},{timeWalking},{timeAll},{failedActivations}";
+                $"{landmarkTypesAmount},{colorsAmount},{silhouettesAmount},{timeRunning},{timeWalking},{timeAll},{failedActivations},{averageAreaDistance}";
         }
     }
 }
--- Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/PathLengthCounter.cs
+++ Assets/Samples/Organica Wild/0.0.1/PipelineSamples/Pipeline/PathLengthCounter.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Framework.Pipeline;
 using Framework.Pipeline.GameWorldObjects;
 using Framework.Pipeline.Geometry;
 using Framework.Pipeline.PipelineGuarantees;
+using Framework.Util.Miscellanous;
+using UnityEngine;
 
 namespace Samples.Organica_Wild._0._0._1.PipelineSamples.Pipeline
 {
@@ -15,6 +18,70 @@ namespace Samples.Organica_Wild._0._0._1.PipelineSamples.Pipeline
 
         public override GameWorld Apply(GameWorld world)
         {
+            List<Area> areas = world.Root.GetAllChildrenOfType<Area>().ToList();
+            int verticesCount = areas.Count;
+            int[,] adjMatrix = new int[verticesCount, verticesCount];
+
+            //initialize all connections as really long
+            for (int j = 0; j < verticesCount; j++)
+            {
+                for (int k = 0; k < verticesCount; k++)
+                {
+                    if (k == j)
+                    {
+                        adjMatrix[j, k] = 0;
+                    }
+                    else
+                    {
+                        adjMatrix[j, k] = FloydWarshall.INF;
+                    }
+                }
+            }
+
+            //set all existing connections
+            for (int index = 0; index < areas.Count; index++)
+            {
+                Area area = areas[index];
+                IEnumerable<AreaConnection> connections = area.GetAllChildrenOfType<AreaConnection>();
+                foreach (AreaConnection areaConnection in connections)
+                {
+                    Area target = areaConnection.Target;
+                    int targetIndex = areas.IndexOf(target);
+                    float distanceToConnection =
+                        (areaConnection.Shape.GetCentroid() - area.Shape.GetCentroid()).magnitude;
+                    float distanceFromConnection =
+                        (areaConnection.Shape.GetCentroid() - target.Shape.GetCentroid()).magnitude;
+                    float distance = distanceFromConnection + distanceToConnection;
+                    adjMatrix[index, targetIndex] = (int) Math.Floor(distance);
+                    adjMatrix[targetIndex, index] = (int) Math.Floor(distance);
+                }
+            }
+
+            int[,] distances = FloydWarshall.Execute(adjMatrix, verticesCount);
+
+            IEnumerable<int> allWithAreaLandmarkIndexes = areas
+                .Where(area => area.GetAllChildrenOfType<Landmark>().Any(landmark => landmark.Shape is OwPolygon))
+                .Select(area => areas.IndexOf(area));
+            
+            int sumOfDistances = 0;
+            int number = 0;
+            foreach (int first in allWithAreaLandmarkIndexes)
+            {
+                foreach (int second in allWithAreaLandmarkIndexes)
+                {
+                    if (first != second)
+                    {
+                        int distanceBetween = distances[first, second];
+                        sumOfDistances += distanceBetween;
+                        number++;
+                    }
+                }
+            }
+
+            float averageOfDistances = sumOfDistances / (float) number;
+            Debug.Log(averageOfDistances);
+            GameManager.analyitcsData.averageAreaDistance = averageOfDistances;
+
             IEnumerable<IGameWorldObject> children = world.Root.GetChildrenInChildren();
             float sum = 0;
             int i = 0;
@@ -31,7 +98,7 @@ namespace Samples.Organica_Wild._0._0._1.PipelineSamples.Pipeline
 
             GameManager.analyitcsData.pathSum = sum;
             GameManager.analyitcsData.pathAverage = sum / i;
-        
+
             return world;
         }
     }
